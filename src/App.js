7d743dcbdece2c1f26337f45dac9effc60340a56
KSnape34 <KSnape16@gmail.com>
2024-11-10 00:55:19 +0000

    rename to pascalcase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Header from "./components/Header";
 import Showcase from "./components/Showcase";
 import Tabs from "./components/Tabs";
-import CTA_contact from "./components/CTA_contact";
+import CTAContact from "./components/CTAContact";
 import Footer from "./components/Footer";
 import FAQs from "./components/FAQs";
-import Download_extensions from "./components/Download_extensions";
+import DownloadExtensions from "./components/DownloadExtensions";
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
     <Header />
     <Showcase />
     <Tabs />
-    <Download_extensions />
+    <DownloadExtensions />
     <FAQs />
-    <CTA_contact />
+    <CTAContact />
     <Footer />
     </>
   );
